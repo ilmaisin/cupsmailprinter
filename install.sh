ef1b5d1c9dcf8161547c986c3b3497aded8bdbd8
Iiro Laiho <iiro.laiho@iki.fi>
2017-04-13 18:56:27 +0300

    finally the path should be ok

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Install script to be used mainly for development use
-cp mailprinter-pdf.ppd /usr/share/cups/ppd/model/mailprinter-pdf.ppd
+cp mailprinter-pdf.ppd /usr/share/cups/model/mailprinter-pdf.ppd
 cp mailprinter /usr/lib/cups/backend/mailprinter
 chmod 755 /usr/lib/cups/backend/mailprinter
 service cups restart
