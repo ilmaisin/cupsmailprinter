039c28a6aa1e3c074cc20ba23bc337a854ea6642
Iiro Laiho <iiro.laiho@iki.fi>
2017-04-13 17:55:26 +0300

    install ppd file to correct location

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Install script to be used mainly for development use
 cp mailprinter-pdf.ppd /usr/share/ppd/cupsfilters/mailprinter-pdf.ppd
-cp mailprinter /usr/lib/cups/backend/mailprinter
+cp mailprinter /usr/lib/cups/model/mailprinter
 chmod 755 /usr/lib/cups/backend/mailprinter
 service cups restart
